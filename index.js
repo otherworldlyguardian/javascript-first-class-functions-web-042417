37db05f08005e4bc879eec6803c4061d6b9e9bce
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-06-06 14:04:02 -0400

    yea empty functions

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback) {
+  callback()
+}
+
+function returnsANamedFunction() {
+  return function hello() {
+  }
+}
+
+function returnsAnAnonymousFunction() {
+  return () => {}
+}
